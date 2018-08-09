e84d1909fc8262d4627413c2babd2a99aa492ce3
ali.qasim <ranaqasim@gmail.com>
2018-08-09 17:56:17 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from 'module/material.module';
 import { AppComponent } from './app.component';
+import {PushmenuComponent} from './components/pushmenu/pushmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PushmenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pushmenu/pushmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pushmenu',
+  templateUrl: './pushmenu.component.html',
+  styleUrls: ['./pushmenu.component.css']
+})
+export class PushmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
